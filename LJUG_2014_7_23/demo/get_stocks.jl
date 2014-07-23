9a17383c7c227b68f4889d4c85bdc8c4b116f4ce
Samuel Colvin <s@muelcolvin.com>
2014-07-23 12:05:41 +0100

    added demos

---
+++ LJUG_2014_7_23/demo/get_stocks.jl
@@ -0,0 +1,25 @@
+using DataFrames
+using Requests
+using HDF5, JLD
+
+function quandl(code, market = :NASDAQ)
+  url = "http://www.quandl.com/api/v1/datasets/GOOG/$(market)_$code.csv?sort_order=asc&trim_start=2012-06-01&trim_end=2014-06-01&auth_token=6YzXnhcc8KU6zC9Y8oqh"
+  response = get(url)
+  sleep(0.5)
+  if response.status != 200
+    error("error getting data\n    url: $url\n     response: $(response.data)")
+  end
+  println("got url: $url\nresponse length: $(length(response.data))")
+  if length(response.data) < 500
+    println(response.data)
+  end
+  readtable(IOBuffer(response.data))
+end
+stockdata = Dict{Symbol, DataFrame}()
+for code in [:AAPL, :GOOG, :FB, :MSFT]
+  df = quandl(code)
+  show(df)
+  println()
+  stockdata[code] = df
+end
+@save "stock_data.jld" stockdata
---
+++ LJUG_2014_7_23/demo/stocks.jl
@@ -0,0 +1,18 @@
+using DataFrames
+using HDF5, JLD
+using Datetime
+using Bokeh
+autoopen(true)
+@load "stock_data.jld"
+
+df = DataFrame(date = map(date, stockdata[:AAPL][:Date]))
+for (name, df2) in stockdata
+  @assert df[:date] == map(date, df2[:Date])
+  df[name] = df2[:Open] ./ df2[1, :Open]
+end
+df[:days] = int(df[:date]) - int(df[1, :date])
+println(eltypes(df))
+
+mat = [df[:AAPL] df[:GOOG] df[:FB] df[:MSFT]]
+
+plot(df[:days], mat; title="stocks")
