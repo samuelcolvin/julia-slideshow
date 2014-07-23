aff66c5718b4d94d4ec906227b44162d7c6f4021
Samuel Colvin <s@muelcolvin.com>
2014-07-23 15:10:40 +0100

    jellyfish bit complete

--- LJUG_2014_7_23/demo/jelly_fish.jl
+++ LJUG_2014_7_23/demo/jelly_fish.jl
@@ -3,8 +3,8 @@ using JellyFish
 #> jaro_winkler("jellyfish","smellyfish") => 0.8962962962962964
 
 looking_for = "Frank Spencer"
-
 names = ["Betty", "Frenk Spancer", "Jessica", "Mrs. Fisher", "Nigel Spencer"]
 jw = map(x -> jaro_winkler(looking_for, x), names)
 found = names[indmax(jw)]
 println("found: $found")
+#> found: Frenk Spancer
