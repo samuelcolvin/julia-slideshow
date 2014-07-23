a65733635daae6a6b6b5d8badafe2c0a2368fc22
Samuel Colvin <s@muelcolvin.com>
2014-07-23 16:30:11 +0100

    added stock correlation

--- LJUG_2014_7_23/demo/stocks.jl
+++ LJUG_2014_7_23/demo/stocks.jl
@@ -2,7 +2,6 @@ using DataFrames
 using HDF5, JLD
 using Datetime
 using Bokeh
-autoopen(true)
 @load "stock_data.jld"
 
 df = DataFrame(date = map(date, stockdata[:AAPL][:Date]))
@@ -16,6 +15,16 @@ println(eltypes(df))
 mat = [df[:AAPL] df[:GOOG] df[:FB] df[:MSFT]]
 
 plot(df[:days], mat; title="stocks", height = 400)
+showplot("time_stocks.html")
 
 println(Bokeh.DEFAULT_GLYPHS_STR)
 println([:AAPL, :GOOG, :FB, :MSFT])
+
+plot(df[:AAPL], df[:GOOG], "bo")
+hold(true)
+plot(df[:AAPL], df[:FB], "ro")
+plot(df[:AAPL], df[:MSFT], "go")
+plot(df[:GOOG], df[:FB], "yo")
+plot(df[:GOOG], df[:MSFT], "ko")
+plot(df[:FB], df[:MSFT], "co"; title="GOOG vs. AAPL, FB, MSFT", height = 400)
+showplot("stock_correlation.html")
